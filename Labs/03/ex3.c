6dcf6696f9c197c0c731ecaa6b9eee575f7e8a48
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-05 10:31:48 +0500

    Create ex3.c

---
+++ Labs/03/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+	float opp, adj;
+
+	printf("Input perpendicular of triangle: ");
+	scanf("%f", &opp);
+
+	printf("Input base of triangle: ");
+	scanf("%f", &adj);
+
+	printf("\n\nHypotenus is %.2lf", sqrt( ((opp * opp) + (adj * adj)) ) );
+}; // end main()
