c096cddb49ef6e826e742e6e6c2ecb1de5c14367
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-05 10:55:28 +0500

    Create ex5.c

---
+++ Labs/03/ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+	int totalDistance, fuelRate;
+
+	printf("Find fuel consumed\n\n");
+
+	printf("Input total distance travelled by vehicle (in km): ");
+	scanf("%d", &totalDistance);
+
+	printf("Input fuel consumption rate of vehicle (in litre): ");
+	scanf("%d", &fuelRate);
+
+	printf("Amount of fuel consumed is %.1f litres", (float) fuelRate * totalDistance);
+}; // end main()
