6455d6f30201eb40f07a7dab66f3a5affae1dfb8
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-05 19:14:05 +0500

    Create ex1.c

---
+++ Labs/03/ex1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+	int num1, num2;
+	char userChoice;
+
+	printf("Operations -> '+', '-', '*', '/' OR press 'E' to exit.\n");
+
+	do {
+		// userChoice
+		printf("Input choice: ");
+		scanf(" %c", &userChoice);
+
+		// exit
+		if (userChoice == 'e' || userChoice == 'E') {
+			break;
+		};
+
+		// num1
+		printf("Input first number: ");
+		scanf("%d", &num1);
+
+		// num2
+		printf("Input second number: ");
+		scanf("%d", &num2);
+
+		if (userChoice == '+') {
+			printf("Answer: %d\n", num1 + num2);
+		} else if (userChoice == '-') {
+			printf("Answer: %d\n", num1 - num2);
+
+		} else if (userChoice == '*') {
+			printf("Answer: %d\n", num1 * num2);
+
+		} else if (userChoice == '/') {
+			printf("Answer: %.1f\n", (float) num1 / num2);
+		};
+		
+	} while(1);
+	
+	return 0;
+}; // end main()
