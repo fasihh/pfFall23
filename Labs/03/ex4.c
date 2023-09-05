32ef242d811c599f40982e24c811ffa5fa8aca15
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-05 10:41:47 +0500

    Create ex4.c

---
+++ Labs/03/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+	int discount, price;
+	
+	printf("Find discount on a price\n\n");
+
+	printf("Input price: ");
+	scanf("%d", &price);
+
+	printf("Input discount (e.g 20): ");
+	scanf("%d", &discount);
+
+	printf("\nPrice after discount: %0.2f", (float) ( (1 - ( (float) discount/100)) * price ) );
+}; // end main()
