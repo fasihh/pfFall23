4d060568a5ab610cb8d6b60392cc025d17268ad8
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-19 18:03:32 +0500

    Update p1.c

--- Labs/05/p1.c
+++ Labs/05/p1.c
@@ -1,3 +1,9 @@
+/*
+ * Creator: Fasih Hasan
+ * Date: 19/9/23
+ * Description: Check if multiple of 3
+ */
+
 #include <stdio.h>
 
 void main() {
@@ -6,11 +12,9 @@ void main() {
 	printf("Input number: ");
 	scanf("%d", &num);
 
-	// (!(num % 3)) ? printf("This number is multiple of 3") : printf("This number is not multiple of 3");
-
 	if (!(num % 3)) {
 		printf("This number is multiple of 3");
 	} else {
 		printf("This number is not multiple of 3");
 	}
-}
+}// end main()
