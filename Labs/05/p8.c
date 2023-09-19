34ac3c42bb7cbd7c08ee1f218494dedad6e7776b
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-19 18:55:19 +0500

    Update p8.c

--- Labs/05/p8.c
+++ Labs/05/p8.c
@@ -1,14 +1,25 @@
+/*
+ * Fasih Hasan
+ * Date: 19/9/23
+ * Description: Give appropriate actions when a value of intensity is given
+ */
+
 #include <stdio.h>
 
 void main() {
-	int intensity, lightSensor;
+	int intensity;
 
+	// taking inputs until proper intensity value
 	do {
 		printf("Input intensity value: ");
 		scanf("%d", &intensity);	
-	} while (intensity < 0);
+	} while (intensity < 0 || intensity > 1000);
 
-	if (intensity > 0 && intensity < 100) {
-
+	if (intensity >= 0 && intensity <= 100) {
+		printf("Evening: low brightness");
+	} else if (intensity > 100 && intensity <= 500) {
+		printf("Lighting: moderate brightness");
+	} else if (intensity > 500) {
+		printf("Sunshine: maximum brightness");
 	}
-}
+} // end main()
