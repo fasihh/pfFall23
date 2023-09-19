0cd8c83356805b3e6d294ec720a36c1cfa67c838
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-19 18:24:43 +0500

    Update p6.c

--- Labs/05/p6.c
+++ Labs/05/p6.c
@@ -1,16 +1,24 @@
+/*
+ * Fasih Hasan
+ * Date: 19/9/23
+ * Description: Check if 7th and 4th bits are on. If yes then turn them off
+ */
+
 #include <stdio.h>
 
 void main() {	
-	int num;
+	int num, bitSeven, bitFour;
 
 	printf("Input 8-bit number: ");
 	scanf("%d", &num);
 
-	if (num / 1000 && num / 1000000) {
+	// finding seventh and fourth bit
+	bitSeven = (num / 1000000) % 10;
+	bitFour = (num / 1000) % 10;
+
+	if (bitSeven && bitFour) {
 		num = num - 1000000 - 1000;
 	} 	
 
 	printf("%d", num);
-}
-
-// 01001000
+} // end main()
