3ee6a5de26be8f5f894ca42f5ab68fc6040e67a1
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-19 19:00:23 +0500

    Update p9.c

--- Labs/05/p9.c
+++ Labs/05/p9.c
@@ -1,3 +1,9 @@
+/*
+ * Fasih Hasan
+ * Date: 19/9/23
+ * Description: Finding roots of an equations by evaluating determinant
+ */
+
 #include <stdio.h>
 #include <math.h>
 
@@ -8,14 +14,16 @@ void main() {
 	printf("Input 'a b c' in this format: ");
 	scanf("%d %d %d", &a, &b, &c); 
 
+	// finding determinant
 	d = b * b - (4 * a * c);	
 
+	// giving appropriate result
 	if (d == 0) {
 		printf("x = %.4f", (float) ( -b + sqrt(d) ) / (2 * a));
 	} else if (d > 0) {
-		printf("x1 = %.4f and x2 = %.4f", (float) ( -b + sqrt(d) ) / (2 * a), ( -b - sqrt(d) ) / (2 * a));
+		printf("x1 = %.4f, x2 = %.4f", (float) ( -b + sqrt(d) ) / (2 * a), (float) ( -b - sqrt(d) ) / (2 * a));
 	} else {
-		printf("no roots");
+		printf("no real roots");
 	}
 
-}// end main
+} // end main()
