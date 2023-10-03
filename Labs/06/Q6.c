657b15fca53a655171eb1ddc4f1f389764d97c69
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-10-03 20:58:17 +0500

    Update Q6.c

--- Labs/06/Q6.c
+++ Labs/06/Q6.c
@@ -1,3 +1,9 @@
+/*
+ * Creator: Fasih Hasan
+ * Date: 10/3/2023
+ * Description: A match stick game between the computer and a user. Make the computer win by having the user pick the last stick
+ */
+
 #include <stdio.h>
 
 int main() {
@@ -6,22 +12,35 @@ int main() {
 
 	while (sticks > 0) {
 		
+		// keep taking input until value is within range
 		do {
 			printf("Enter number of sticks (1, 2, 3 or 4): ");
 			scanf("%d", &n);
-		} while (n > 4 || n < 1 && sticks < n);
+		} while (n > 4 || n < 1);
 		sticks -= n;
 
-		if (sticks < 5) {
-			while (sticks > 1) {
-				sticks--;
-			}
-		} else {
-			sticks -= 3;
-		}
+		// computer always subtracts alternate number in the list. if user picks 4 then 1, 3 then 2, 2 then 3 etc
+		switch (n) {
+			case 4:
+				sticks -= 1;
+				break;
+			case 3:
+				sticks -= 2;
+				break;
+			case 2:
+				sticks -= 3;
+				break;
+			case 1:
+				sticks -= 4;
+				break;
+		}		
 
-		printf("Sticks left %d\n", sticks);
+		// don't print negative value for sticks at the end
+		if (sticks >= 0) 
+			printf("Sticks left %d\n", sticks);
+		else 
+			printf("Sticks left 0");
 	}
 
 	return 0;
-}
+} // end main()
