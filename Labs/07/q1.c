ecc089b51179e9040bb0019b4811fee9cc0f5bb3
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-10-10 08:36:48 +0500

    Update q1.c

--- Labs/07/q1.c
+++ Labs/07/q1.c
@@ -6,20 +6,26 @@
 
 #include <stdio.h>
 
+int divide(int, int);
+
 int main() {
     int num1, num2;
     printf("Enter num1/num2: ");
     scanf("%d/%d", &num1, &num2);
 
+    printf("ans: %d", divide(num1, num2));
+
+    return 0;
+} // end main()
+
+int divide(int num1, int num2) {
     int c = 0;
     // subtracting num1 with num2 until 0
     while (num1 > 0) {
+        if (num1 < num2) break;
         // the amount of times num1 needs to be subtracted by num2
         c++;
         num1 -= num2;
     }
-
-    printf("ans: %d", c);
-
-    return 0;
-} // end main()
+    return c;
+} // end divide(int, int)
