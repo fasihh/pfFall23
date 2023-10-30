403e8d412aa7232e15cd0fd4c8fcd287bdec38ca
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-10-30 12:51:02 +0500

    Update q3.c

--- Labs/08/q3.c
+++ Labs/08/q3.c
@@ -26,9 +26,3 @@ int main() {
 
     return 0;
 } // main()
-
-// thing. i has to be initialized with -1
-int count_rec(char *s, char c, int i) 
-{
-    return (s == NULL) ? (!s) ? i+1 : i : count_rec(strchr(s+1, c), c, i+1);
-}
