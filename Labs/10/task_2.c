ab69ac90c6f0d4460ec66f9625adc8f5a3fa91de
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-11-15 21:32:19 +0500

    Update task_2.c

--- Labs/10/task_2.c
+++ Labs/10/task_2.c
@@ -11,10 +11,10 @@ void swap(void *n1, void *n2, char type)
 	// create a XOR mask for both values
 	// XOR the mask with each value to swap it
 
-	if (type == 'd')  {
-		int mask = *(int *)n1 ^ *(int *)n2;
-		*(int *)n1 ^= mask, *(int *)n2 ^= mask;
-	} else if (type == 'c')  {
+	if (type == 'd' || type == 'f' || type == 'l') {
+		long mask = *(long *)n1 ^ *(long *)n2;
+		*(long *)n1 ^= mask, *(long *)n2 ^= mask;
+	} else if (type == 'c') {
 		char mask = *(char *)n1 ^ *(char *)n2;
 		*(char *)n1 ^= mask, *(char *)n2 ^= mask;
 	}
@@ -24,6 +24,7 @@ int main()
 {
 	int num1 = 3, num2 = 1;
 	char char1 = 'A', char2 = 'B';
+	float float1 = 3.5, float2 = 1.5;
 
 	// pass void * casted pointer
 	printf("%d %d ", num1, num2);
@@ -32,7 +33,11 @@ int main()
 
 	printf("%c %c ", char1, char2);
 	swap((void *) &char1, (void *) &char2, 'c');
-	printf("swaps %c %c", char1, char2);
+	printf("swaps %c %c\n", char1, char2);
+
+	printf("%.1f %.1f ", float1, float2);
+	swap((void *) &float1, (void *) &float2, 'f');
+	printf("swaps %.1f %.1f", float1, float2);
 
 	return 0;
 } // end main()
