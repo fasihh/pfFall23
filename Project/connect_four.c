9bc9cf6b3a15c05f52621cd8ce349acaa494925e
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-11-23 22:28:23 +0500

    Update connect_four.c

--- Project/connect_four.c
+++ Project/connect_four.c
@@ -1,53 +1,57 @@
+/*
+ * Creators: Fasih Hasan, Syed Ukkashah, Muhammad Ali
+ * Date (Last changed): 23/11/23
+ * Desc: A game called connect 4. First player to create a sequence of 4 characters in a row in a 6 by 7 grid will win
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <Windows.h>
 
-#define BUFFER_SIZE 10
-#define f_name "wins.txt"
+#define P1 'R'
+#define P2 'B'
+#define X '.'
 
-// default settings
-const char X = ' ', P1 = 'R', P2 = 'B';
 const int rows = 6, cols = 7;
 
-
 // create a game grid
-char **create_game_grid();
+char **create_game_grid(); // by Ali, modi
+
+// uses the window lib to color the text
+void color_char(char player); // by Fasih
 
 // print the game grid
-void print_grid(char **grid);
+void print_grid(char **grid); // by Ali, modified by Fasih and Ukkashah
 
 // update the frame by clearing the screen and printing the grid
-void update_frame(char **grid);
+void update_frame(char **grid); // by Fasih
 
 // user input for the column number
-int user_input(int player);
+int user_input(int player); // by Ukkashah
 
 // validate a move in the specified column
-bool validate_move(char **grid, int col);
+bool validate_move(char **grid, int col); // by Fasih
 
 // get the first empty row in a column
-int get_first_empty(char **grid, int col);
+int get_first_empty(char **grid, int col); // by Ukkashah
 
 // set the grid at the specified column and row with the player's character
-void set_grid(char **grid, int col, int player);
+void set_grid(char **grid, int col, int player); // by Ukkashah
 
 // check the win status of the game after a move
-bool game_win_status(char **grid, int col, int player);
-
-// check if the game is a draw
-bool game_draw_status(char **grid);
+bool game_win_status(char **grid, int col, int player); // by Fasih, Ali and Ukkashah
 
 // displays relevant game end screen to player
-void game_end_display(char **grid, int player, bool type);
+void game_end_display(char **grid, int player, bool type); // by Ali
 
-// extracts player wins from the wins file
-int *save_win(int player);
+/*
+		to-do:
 
-// shows the wins for each player
-void display_wins(int player);
-
+- draw condition
+- save wins and display them at the end
+*/
 
 int main()
 {
@@ -55,17 +59,11 @@ int main()
 	bool valid = true, win = false;
 	char player_char, **grid = create_game_grid();
 
+	// default color scheme
+	system("Color 0E");
+
 	// game loop
 	while (!win) {
-
-		// check at the start if draw
-		// this was done to avoid out of bound errors mid game
-		if (game_draw_status(grid)) {
-			// display end screen for DRAW
-			game_end_display(grid, player, false);
-			return 0;
-		}
-
 		// for each player
 		for (player = 0; player < 2; player++) {
 			// loop until valid input
@@ -82,13 +80,20 @@ int main()
 			// check if win or draw
 			win = game_win_status(grid, col, player);
 
-			if (win) break;
+			if (win) {
+				break;
+			}
 		}
 	}
 	
 	// display screen for WIN of player
-	game_end_display(grid, player, true);
-	return 0;
+	update_frame(grid);
+	color_char((player) ? P1 : P2);
+	printf(" WINS");
+
+	// resetting color scheme
+	system("Color 07");
+	return 0; // end
 } // end main()
 
 
@@ -98,26 +103,69 @@ char **create_game_grid()
 	char **grid = (char **)malloc(sizeof(char *) * rows);
 
 	// creates single pointers and sets them in all rows
-	for (int i = 0; i < rows; i++) grid[i] = (char *)malloc(sizeof(char) * cols);
+	for (int i = 0; i < rows; i++) {
+		grid[i] = (char *)malloc(sizeof(char) * cols);
+	}
 
 	// initializing as default
-	for (int i = 0; i < rows; i++) memset(grid[i], X, cols);
+	for (int i = 0; i < rows; i++) {
+		memset(grid[i], X, cols);
+	}
 
 	return grid;
 } // end create_game_grid()
 
+void color_char(char player)
+{
+	switch (player) {
+        case P1:
+        	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+            printf("%c", player); // Red color for P1
+            break;
+        case P2:
+        	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
+            printf("%c", player); // Blue color for P2
+            break;
+        default:
+            printf("%c", player);
+    }
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+} // end color_char()
 
 void print_grid(char **grid)
 {
-	printf("%-2c", ' ');
-	for (int i = 0; i < cols; i++) printf("%-4d", i+1);
+	
+	printf("%-4c", ' ');
+	for (int i = 0; i < cols; i++) {
+		printf("%-4d", i+1);
+	}
+	printf("\n");
+	printf("%-2c|", ' ');
+	for (int i = 2; i <= cols*4; i++) printf("%c", '-');
+	printf("|");
+
 	printf("\n");
 	for (int i = 0; i < rows; i++) {
-		printf("%-2d", i+1);
-		for (int j = 0; j < cols; j++)
-			printf("%-4c", grid[i][j]);
-		printf("\n\n");
+		printf("%d | ", i+1);
+		for (int j = 0; j < cols; j++) {
+			if (j == cols - 1) {
+				color_char(grid[i][j]);
+				printf(" |");
+				break;
+			}
+			color_char(grid[i][j]);
+			printf("%-3c", ' ');
+		}
+		if (i == rows - 1) continue;
+		printf("\n");
+		printf("%3c", '|');
+		for (int i = 2; i < cols*4; i++) printf("%c", ' ');
+		printf(" |\n");
 	}
+	printf("\n");
+	printf("%-2c|", ' ');
+	for (int i = 2; i <= cols*4; i++) printf("%c", '-');
+	printf("|\n\n");
 } // end print_grid(Player **)
 
 
@@ -174,117 +222,45 @@ bool game_win_status(char **grid, int col, int player)
 	// down
 	if (row + 3 < rows) {
 		if (grid[row+1][col] == player_char && grid[row+2][col] == player_char && grid[row+3][col] == player_char)
-				return true;
+			return true;
 	}
 
 	// right
 	if (col + 3 < cols) {
 		if (grid[row][col+1] == player_char && grid[row][col+2] == player_char && grid[row][col+3] == player_char)
-				return true;
+			return true;
 	}
 
 	// left
 	if (col - 3 >= 0) {
 		if (grid[row][col-1] == player_char && grid[row][col-2] == player_char && grid[row][col-3] == player_char)
-				return true;
+			return true;
 	}
 
 	// up right
 	if (col + 3 < cols && row - 3 >= 0) {
 		if (grid[row-1][col+1] == player_char && grid[row-2][col+2] == player_char && grid[row-3][col+3] == player_char)
-				return true;
+			return true;
 	}
 
 
 	// up left
 	if (col - 3 >= 0 && row - 3 >= 0) {
 		if (grid[row-1][col-1] == player_char && grid[row-2][col-2] == player_char && grid[row-3][col-3] == player_char)
-				return true;
+			return true;
 	}
 
 	// down right
 	if (col + 3 < cols && row + 3 < rows) {
 		if (grid[row+1][col+1] == player_char && grid[row+2][col+2] == player_char && grid[row+3][col+3] == player_char)
-				return true;
+			return true;
 	}
 
 	// down left
 	if (col - 3 >= 0 && row + 3 < rows) {
 		if (grid[row+1][col-1] == player_char && grid[row+2][col-2] == player_char && grid[row+3][col-3] == player_char)
-				return true;
+			return true;
 	}
 
 	return false;
 } // end game_win_status()
-
-
-bool game_draw_status(char **grid)
-{
-	// if top row is not full return false
-	for (int i = 0; i < cols; i++) if (grid[0][i] == X) return false;
-	return true;
-} // end game_draw_status()
-
-
-void game_end_display(char **grid, int player, bool type)
-{
-	update_frame(grid);
-	(type) ? printf("%c WINS", (player) ? P1 : P2) : printf("DRAW");
-	display_wins(player);
-}
-
-
-int *save_win(int player)
-{
-	char line[BUFFER_SIZE], player1[BUFFER_SIZE], player2[BUFFER_SIZE];;
-	int *wins = (int *)calloc(2, sizeof(int));
-	FILE *f_ptr = fopen(f_name, "r+");
-
-	// if file exists
-	if (f_ptr != NULL) {
-		for (int i = 0;!feof(f_ptr); i++) {
-			// reading win from file
-			fgets(line, BUFFER_SIZE, f_ptr);
-
-			// trimming string
-			line[strcspn(line, "\n")] = 0;
-
-			// adding into win of the player
-			// ignoring if -1 (DRAW)
-			wins[i] = atoi(line) + (player != -1 && i == player);
-		}
-
-		fclose(f_ptr);
-	} else wins[player] = 1; // setting to 1 if no file existed
-
-	// ending if DRAW
-	if (player == -1) return wins;
-
-	// writing new wins in file
-
-	f_ptr = fopen(f_name, "w");
-
-	// if file can be opened
-	if (f_ptr != NULL) {
-		// adding wins to the line buffer
-		sprintf(line, "%d\n%d", wins[0], wins[1]);
-
-		// writing the lines in to the file
-		fprintf(f_ptr, line);
-
-		fclose(f_ptr);
-	}
-
-	return wins;
-} // end save_win()
-
-
-void display_wins(int player)
-{
-	// get wins from the file
-	int *wins = save_win(player);
-	printf("\nTotal %c wins: %d\nTotal %c wins: %d", P1, wins[1], P2, wins[0]);
-
-	// to not immediately close the program at the end
-	Sleep(10000);
-} // end display_wins()
