af4caa7e868ed0bab93f415138c405a5edd43b9b
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-11-28 09:15:35 +0500

    Update task1.c

--- Labs/12/task1.c
+++ Labs/12/task1.c
@@ -9,37 +9,45 @@
 #include <stdlib.h>
 
 #define MAX_BUFFER_SIZE 100
+#define MAX_FILE_SIZE 500
 
-void format_line(char str[100])
+int format_line(char str[100])
 {
     char delim[2] = ",";
+    int rows = 0;
     char *row = strtok(str, delim);
 
     // separating words with commas
     while (row != NULL) {
+        rows++;
         row[strcspn(row, "\n")] = 0; // removing new line from the end of file line
         printf("%-14s | ", row); // printing in correct format
         row = strtok(NULL, delim);
     }
 
     printf("\n");
+    return rows;
 } // end format_line()
 
 void read_file(FILE *fptr)
 {
     char buffer[MAX_BUFFER_SIZE];
+    int cols = 0, rows = 0;
 
     // reading all lines
     while (!feof(fptr)) {
-        format_line(fgets(buffer, MAX_BUFFER_SIZE, fptr)); 
+        rows = format_line(fgets(buffer, MAX_BUFFER_SIZE, fptr)); 
+        cols++;
     }
-} // end read_table_file()
+
+    printf("\n\nrows: %d, cols: %d", rows-1, cols);
+} // end read_stable_file()
 
 int main()
 {
     FILE *fptr = fopen("table.csv", "r");
     read_file(fptr);
     fclose(fptr);
-    
+
     return 0;
 } // end main()
